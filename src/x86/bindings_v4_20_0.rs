3b99ee1b252757e163a7facd36bf388593ac5693
Samuel Ortiz <sameo@linux.intel.com>
2020-02-17 16:22:58 +0100

    x86: Derive serde traits for kvm_msr_entry
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- src/x86/bindings_v4_20_0.rs
+++ src/x86/bindings_v4_20_0.rs
@@ -2090,6 +2090,7 @@ fn bindgen_test_layout_kvm_fpu() {
 }
 #[repr(C)]
 #[derive(Debug, Default, Copy, Clone, PartialEq)]
+#[cfg_attr(feature = "with-serde", derive(Deserialize, Serialize))]
 pub struct kvm_msr_entry {
     pub index: __u32,
     pub reserved: __u32,
